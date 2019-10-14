0511860a83131ca5ca372ca0076f810b0c703cde
wakanyan <kyoto.kobe.1126.yuto@gmail.com>
2019-10-14 17:15:03 +0900

    original App

--- OriginalApp3/EditViewController.swift
+++ OriginalApp3/EditViewController.swift
@@ -7,62 +7,83 @@
 //
 
 import UIKit
+import RealmSwift
 
-    class EditViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class EditViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UIScrollViewDelegate {
+    
+    
+    @IBOutlet weak var nameTextFeild: UITextField!
+    var testText:String = "default"
+    //②UserDefaults のインスタンス
+    let userDefaults = UserDefaults.standard
+    
+    
+    
+    @IBOutlet weak var nicknameTextFeild: UITextField!
+    
+    @IBOutlet weak var affilicationtextField: UITextField!
+    
+    @IBOutlet weak var SNSTextField: UITextField!
+    var scrollView1 = UIScrollView()
+    
+    @IBOutlet weak var anotherTextField: UITextField!
+    var scrollView2 = UIScrollView()
+    
+    
+    @IBOutlet var cameraImageView: UIImageView!
+  
 
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController ,didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        cameraImageView.image = info[.editedImage] as? UIImage
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func launchcamera() {
+        let ipc = UIImagePickerController()
+        ipc.delegate = self
+        ipc.sourceType = UIImagePickerController.SourceType.photoLibrary
+        //編集を可能にする
+        ipc.allowsEditing = true
+        self.present(ipc,animated: true, completion: nil)
         
-        @IBOutlet weak var nameTextFeild: UITextField!
-        
-        @IBOutlet weak var nicknameTextFeild: UITextField!
-        
-        @IBOutlet weak var affilicationtextField: UITextField!
-        
-        @IBOutlet weak var SNSTextField: UITextField!
-        
-        @IBOutlet weak var anotherTextField: UITextField!
-        
-        
-        @IBOutlet var cameraImageView: UIImageView!
-        
-        var originalImage : UIImage!
-        
-        func imagePickerController(_ picker: UIImagePickerController ,didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-            cameraImageView.image = info[.editedImage] as? UIImage
-            dismiss(animated: true, completion: nil)
+    }
+    func ImagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        //編集機能??
+        if info[.originalImage] != nil {
+            let image = info[UIImagePickerController.InfoKey.editedImage] as! UIImage
+            //画像を設定する
+            cameraImageView.image = image
         }
+        dismiss(animated: true,completion: nil)
         
-        
-        @IBAction func launchcamera() {
-            if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
-                let picker = UIImagePickerController()
-                picker.sourceType = .photoLibrary
-                picker.delegate = self
-                
-                picker.allowsEditing = true
-                
-                present(picker, animated: true, completion: nil)
-            }
-            
-         
-        }
-        
-        
-        
-        
-        
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            // Do any additional setup after loading the view.
+    }
+    
 
-            
-        }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        nameTextFeild.delegate = self
+        nicknameTextFeild.delegate = self
+        affilicationtextField.delegate = self
+        SNSTextField.delegate = self
+        anotherTextField.delegate = self
+    }
+    
 }
-
 // Returnでキーボードを閉じる
 extension EditViewController: UITextFieldDelegate {
-
-    func nameTextFieldShouldReturn(_ nameTextField: UITextField) -> Bool {
-
+    
+    func textFieldShouldReturn(_ nameTextField: UITextField) -> Bool {
+        
+        nameTextField.resignFirstResponder()
+        // キーボードを閉じるよ！！
         return true
     }
+    
 }
